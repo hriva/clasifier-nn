0ec3220c5aefbde1d87dcd3b15efa7290c6cb3b1
hector.riv <books_thief@outlook.com>
2024-11-10 15:00:24 -0600

    ref: move split to training stage

--- src/wrangle.py
+++ src/wrangle.py
@@ -13,20 +13,8 @@ df = df.drop_duplicates(text_col)
 sample_count = df.groupby(label_col).count()
 under_sampled = int(sample_count[text_col].min())
 under_sampled
+grouped_df = df.groupby(label_col).sample(n=50, random_state=42)  # suffle group
+grouped_df.reset_index(drop=True, inplace=True)
 
-# Sampling
-# splitter = GroupShuffleSplit(test_size=0.20, n_splits=2, random_state=7)
-# split = splitter.split(df, groups=df["Group_Id"])
-# train_inds, test_inds = next(split)
-#
-# train = df.iloc[train_inds]
-# test = df.iloc[test_inds]
-
-grouped_df = df.groupby(label_col).sample(n=15, random_state=42)  # suffle group
-
-# split
-train = grouped_df.groupby(label_col).head(10)
-test = grouped_df.groupby(label_col).tail(5)
-
-train.to_csv("data/train.csv", index=False)
-test.to_csv("data/test.csv", index=False)
+# dump
+grouped_df.to_parquet("data/data.parquet")
